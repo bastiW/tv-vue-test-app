8e015e6204efec8da05939939562b00a58b099ea
Sebastian Weiss <sebastian.weiss@capgemini.com>
2021-07-03 12:33:12 +0200

    add capacitor android

---
+++ android/app/src/main/java/tvvuetestapp/com/MainActivity.java
@@ -0,0 +1,5 @@
+package tvvuetestapp.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tvvuetestapp.com',
+  appName: 'tvvuetestapp',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
